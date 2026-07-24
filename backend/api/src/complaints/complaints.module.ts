3f1452b9360c286b8da62c916c1c340e231a623d
Rojin Roy <rojinroy370@gmail.com>
2026-07-24 21:25:42 +0530

    feat: wire up ComplaintsModule dependencies

---
+++ backend/api/src/complaints/complaints.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { ComplaintsController } from './complaints.controller';
+import { ComplaintsService } from './complaints.service';
+import { PrismaModule } from '../prisma/prisma.module';
+
+@Module({
+  imports: [PrismaModule],
+  controllers: [ComplaintsController],
+  providers: [ComplaintsService],
+  exports: [ComplaintsService],
+})
+export class ComplaintsModule {}
